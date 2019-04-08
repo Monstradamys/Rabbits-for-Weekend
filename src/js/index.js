248fd30aab827a5636ce3ac5d92c2471ae42e79a
Monstradamys <dmitrieva.av1337@gmail.com>
2019-04-08 22:20:05 +0300

    github pages

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../scss/styles.scss';
-
-import App from './components/app.js';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
